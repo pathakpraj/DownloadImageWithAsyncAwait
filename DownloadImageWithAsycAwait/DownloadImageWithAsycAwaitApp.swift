508af50ad671e900698169abf82018f7ae1d453f
Prajakta Pathak <prajakta.coding.email@gmail.com>
2025-12-17 08:46:56 -0500

    Initial Commit

---
+++ DownloadImageWithAsycAwait/DownloadImageWithAsycAwaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  DownloadImageWithAsycAwaitApp.swift
+//  DownloadImageWithAsycAwait
+//
+//  Created by Prajakta Pathak on 12/17/25.
+//
+
+import SwiftUI
+
+@main
+struct DownloadImageWithAsycAwaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
